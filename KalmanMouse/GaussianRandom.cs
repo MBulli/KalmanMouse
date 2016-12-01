15336f00260c37955c881ba1598e3b10f0863c19
MBulli <markusbullmann@gmail.com>
2016-12-01 20:15:17 +0100

    Initial commit

---
+++ KalmanMouse/GaussianRandom.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class GaussianRandom
+    {
+        private readonly Random rand;
+
+        public readonly double Mean;
+        public readonly double StandardDeviation;
+
+        public GaussianRandom(double mean = 0, double standardDeviation = 1, int? seed = null)
+        {
+            Mean = mean;
+            StandardDeviation = standardDeviation;
+            rand = seed == null ? new Random() : new Random(seed.Value);
+        }
+
+        public double NextDouble()
+        {
+            // these are uniform(0,1) random doubles
+            double u1 = rand.NextDouble(); 
+            double u2 = rand.NextDouble();
+
+            // random normal(0,1)
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); 
+
+            // random normal(mean,stdDev^2)
+            double randNormal = Mean + StandardDeviation * randStdNormal;
+
+            return randNormal;
+        }
+    }
+}
---
+++ KalmanMouse/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MathNet.Numerics.LinearAlgebra;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GaussianRandom rnd;
+
+        // [x y x' y']
+        Vector<double> Xprev = Vector<double>.Build.Dense(4);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            rnd = new GaussianRandom(mean: 0, standardDeviation: 5);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            double dt = 0.1;
+
+            var A = Matrix<double>.Build.DenseOfArray(new double[,] {
+                { 1, 0, dt, 0 },
+                { 0, 1, 0,  dt},
+                { 0, 0, 1,  0 },
+                { 0, 0, 0,  1 }
+            });
+
+            var B = Vector<double>.Build.DenseOfArray(new[] {
+                0.5 * dt*dt,
+                0.5 * dt*dt,
+                dt,
+                dt
+            });
+
+            // No acceleration
+            double u = 0;
+            double w = 0;
+
+            var Xk = A * Xprev + B * u + w;
+
+            Point pos = e.GetPosition(inkCanvas);
+            pos.Offset(rnd.NextDouble(), rnd.NextDouble());
+
+            inkCanvas.Strokes.Add(new Stroke(new StylusPointCollection(new[] { pos })));
+            if (inkCanvas.Strokes.Count > 50)
+            {
+                inkCanvas.Strokes.RemoveAt(0);
+            }
+        }
+    }
+}
